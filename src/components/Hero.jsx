1a250a9f7ec4607601dbf107fd34b4299d692005
nzube2 <nzubemolokwu5@gmail.com>
2026-09-10 09:18:54 +0100

    resume link change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
               View Case Studies
             </a>
             <a
-              href="https://drive.google.com/file/d/1pclocrpsM3sVOQq0Cg0433XQNfhL-8JJ/view?usp=sharing"
+              href="https://drive.google.com/file/d/1cszYHW1Ue_q-ujQttQjjPFOSuHK2Zpk7/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="btn-secondary"
